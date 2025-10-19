d707fe9571a926b2718becbdef76599fa0b8c6e4
EnderLumi <jonas.g.schoenherr@outlook.de>
2025-10-20 00:34:27 +0200

    i wont even bother writhing commit messages anymore. I AM SO ANONYED BY WINDOWS

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 # ruft beim Build oder pip install den Windows-Launcher-Installer auf
 if sys.platform == "win32":
     try:
-        from localchat.installer_windows import post_install
+        from installer_windows import post_install
         post_install()
     except Exception as e:
         print(f"[localchat installer warning] {e}")

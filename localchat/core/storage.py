4e2886b031d81fa577b7164db29758d03869e8d5
leon <leosi@DESKTOP>
2025-11-16 21:57:09 +0100

    updated core/storage

--- localchat/core/storage.py
+++ localchat/core/storage.py
@@ -5,12 +5,13 @@ import platform
 from pathlib import Path
 import random
 import tempfile
+from typing import Any
+from collections.abc import Callable
 
-class _Unknown: ...
+from package import Package, PackageFactory, UserNamePackageFieldType, ColorPackageFieldType
+from utils import ConcurrentLazy
 
-_GET_APP_DATA_DIR__PATH : str|None|_Unknown = _Unknown()
-
-def _get_app_data_dir__init() -> str|None:
+def _app_data_dir() -> str|None:
     if os.name == "nt": # Windows
         return os.getenv("APPDATA")
     if platform.system() == "Darwin": # MacOS
@@ -22,52 +23,123 @@ def _get_app_data_dir__init() -> str|None:
             return str(Path.home()) + "/.local/share"
     return None
 
-def get_app_data_dir() -> str|None:
-    global _GET_APP_DATA_DIR__PATH
-    if isinstance(_GET_APP_DATA_DIR__PATH, _Unknown):
-        _GET_APP_DATA_DIR__PATH = _get_app_data_dir__init()
-    return _GET_APP_DATA_DIR__PATH
+"""
+Variable that can be used to acces the OS specific app data directory.
+"""
+app_data_dir = ConcurrentLazy(_app_data_dir)
 
-_GET_STORAGE_DIR__PATH : str|None|_Unknown = _Unknown()
+def get_app_data_dir() -> str|None:
+    """
+    Returns the directory path to this operating
+    systems equiavalent to %APPDATA% on Windows.
+    Returns None in case this OS is not supported.
+    (Supported operating systems: Windows, macOS, Android, Ubuntu, Linux in general (not all distros tested))
+    """
+    return app_data_dir.get()
+
+def _storage_dir() -> str|None:
+    p = app_data_dir.get()
+    if p != None:
+        p += "/jllc"
+        os.makedirs(p,exist_ok=True)
+    return p
+
+"""
+Variable that can be used to acces the OS specific storage directory.
+This is where any save files of this app are stored.
+"""
+storage_dir = ConcurrentLazy(_storage_dir)
 
 def get_storage_dir() -> str|None:
-    global _GET_STORAGE_DIR__PATH
-    if isinstance(_GET_STORAGE_DIR__PATH,_Unknown):
-        r = get_app_data_dir()
-        if r != None:
-            r += "/jllc"
-            os.makedirs(r,exist_ok=True)
-        _GET_STORAGE_DIR__PATH = r
-    assert(not isinstance(_GET_STORAGE_DIR__PATH,_Unknown))
-    return _GET_STORAGE_DIR__PATH
+    """
+    Returns the directory path to where
+    any save data of this app should be
+    saved.
+    Returns None in case this OS is not supported.
+    """
+    return storage_dir.get()
 
+OPTIONS_PACKAGE_FACTORY = PackageFactory(set([
+    UserNamePackageFieldType("user.name"),
+    ColorPackageFieldType("user.name.color",True),
+    ]))
 
-_USER_NAME : str|None = None
+class StoredPackage: ...
 
-def _get_user_name_filename() -> str|None:
-    storage_dir = get_storage_dir()
-    return storage_dir + "/username.txt" if storage_dir != None else None
+class StoredPackage(Package):
+    def __init__(self, factory, onChange : Callable[[StoredPackage],None]):
+        super().__init__(factory)
+        self._onChange = onChange
 
-def set_user_name(username : str):
-    global _USER_NAME
-    if username == _USER_NAME: return
-    _USER_NAME = username
-    filename_real = _get_user_name_filename()
-    if filename_real == None: return
+    def set_on_change(self, onChange : Callable[[StoredPackage],None]):
+        self._onChange = onChange
+    def get_on_change(self, onChange : Callable[[StoredPackage],None]):
+        return self._onChange
+
+    def __setitem__(self, key : str, value : Any):
+        super().__setitem__(key,value)
+        self._onChange(self)
+    def __delitem__(self, key : str) -> None:
+        super().__delitem__(key)
+        self._onChange(self)
+
+def _load_options() -> Package:
+    """
+    Loads the users options as a package.
+    """
+    options = StoredPackage(OPTIONS_PACKAGE_FACTORY, lambda x: None)
+    if storage_dir.get() == None:
+        print("""waring: storage operations are not supported for this operating system.
+Any changes made will not be saved.""")
+        return options
+    options_file_name = storage_dir.get() + "/options.bin"
+    if os.access(options_file_name,os.R_OK):
+        try:
+
+            with open(options_file_name,"rb") as options_file:
+                options.deserialize(options_file)
+        except (IOError, EOFError) as error:
+            ignored = input(f"""An error occured while reading your options from '{options_file_name}'.
+Press enter to reset your options.
+Exit the application if you don't want your options to be resetted.
+""")
+            os.remove(options_file_name)
+    elif (old_username := _load_user_name()) != None:
+        options.set_on_change(_save_options)
+        try:
+            options["user.name"] = old_username
+        except ValueError:
+            ...
+    options.set_on_change(_save_options)
+    return options
+
+def _save_options(options : Package):
+    """
+    Saves the users options as a package.
+    """
+    if storage_dir.get() == None: return
+    options_file_name = storage_dir.get() + "/options.bin"
     file_temp = tempfile.NamedTemporaryFile(delete=False)
     filename_tmp = file_temp.name
     try:
-        file_temp.write(username.encode("utf-8"))
+        options.serialize(file_temp)
         file_temp.close()
-        os.replace(filename_tmp,filename_real)
+        os.replace(filename_tmp,options_file_name)
     except:
+        if not file_temp.closed:
+            try: file_temp.close()
+            except: ...
         if os.access(filename_tmp, os.F_OK):
             os.remove(filename_tmp)
 
+def _get_user_name_filename() -> str|None:
+    return storage_dir.get() + "/username.txt" if storage_dir.get() != None else None
+
+def set_user_name(username : str):
+    options["user.name"] = username
+
 def get_user_name() -> str:
-    username = _USER_NAME
-    assert(username != None)
-    return username
+    return options["user.name"]
 
 def _load_user_name() -> str|None:
     filename = _get_user_name_filename()
@@ -75,22 +147,16 @@ def _load_user_name() -> str|None:
     if not os.access(filename, os.F_OK | os.R_OK): return None
     return str(Path(filename).read_bytes(),"utf-8")
 
-_USER_NAME = _load_user_name()
-if _USER_NAME is None:
-    user_input = input("Choose your username: ").strip()
-    if not user_input:
-        set_user_name(f"New User {random.randint(0,0xFFFF)}")
-    set_user_name(user_input)
-
+options = _load_options()
 
 if __name__ == "__main__":
     print("starting tests: localchat/core/storage")
     print(f"system: {platform.system()}")
     print(f"app data dir: {get_app_data_dir()}")
-    print(f"storage dir: {get_storage_dir()}")
-    print(f"user name: {get_user_name()}")
-    new_user_name = f"New User {random.randint(0,0xFFFF)}"
+    print(f"storage dir: {storage_dir.get()}")
+    print(f"user name: {options['user.name'] if 'user.name' in options else 'N/A'}")
+    new_user_name = input("username:") #f"New User {random.randint(0,0xFFFF)}"
     print(f"attempting to set user name to: {new_user_name}")
-    set_user_name(new_user_name)
-    print(f"updated user name: {get_user_name()}")
+    options["user.name"] = new_user_name
+    print(f"updated user name: {options['user.name']}")
     print("finished tests: localchat/core/storage")
